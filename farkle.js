9374de9bc7fa00db4753a6531961ced499c617a0
Daniel Hensler <henslerdl@gmail.com>
2020-10-07 09:03:58 -0600

    Added 6 dice, organized further.

--- farkle.js
+++ farkle.js
@@ -1,69 +1,164 @@
 let rollDiceEl = document.getElementById("rollDice")
 
-let rollTotEl = document.getElementById("rollTot")
+let resetAllEl = document.getElementById("resetAll")
 
-let resetScoreEl = document.getElementById("resetScore")
-
-let die1El = document.getElementById("die1")
-let die2El = document.getElementById("die2")
+let die1AEl = document.getElementById("die1A")
+let die2AEl = document.getElementById("die2A")
+let die3AEl = document.getElementById("die3A")
+let die4AEl = document.getElementById("die4A")
+let die5AEl = document.getElementById("die5A")
+let die6AEl = document.getElementById("die6A")
 
 let roll1
 let roll2
+let roll3
+let roll4
+let roll5
+let roll6
 
-let score
+let roll1Saved = false
+let roll2Saved = false
+let roll3Saved = false
+let roll4Saved = false
+let roll5Saved = false
+let roll6Saved = false
+
+let score1 = 0
+let score2 = 0
+
+let currentScore = 0
 
 function getRandomInt (max) {
     return Math.floor(Math.random() * Math.floor(max))
 }
 
 function rollDice () {
-    die1El.classList.remove("remove")
-    die2El.classList.remove("remove")
-    resetScoreEl.classList.remove("remove")
-    roll1 = getRandomInt(6) + 1
-    roll2 = getRandomInt(6) + 1
-    score = roll1 + roll2
+    die1AEl.classList.remove("remove")
+    die2AEl.classList.remove("remove")
+    die3AEl.classList.remove("remove")
+    die4AEl.classList.remove("remove")
+    die5AEl.classList.remove("remove")
+    die6AEl.classList.remove("remove")
+    resetAllEl.classList.remove("remove")
+    if (!roll1Saved) {
+        roll1 = getRandomInt(6) + 1
+        console.log("Rolling die 1")
+    }
+    if (!roll2Saved) {
+        roll2 = getRandomInt(6) + 1
+        console.log("Rolling die 2")
+    }
+    if (!roll3Saved) {
+        roll3 = getRandomInt(6) + 1
+        console.log("Rolling die 3")
+    }
+    if (!roll4Saved) {
+        roll4 = getRandomInt(6) + 1
+        console.log("Rolling die 4")
+    }
+    if (!roll5Saved) {
+        roll5 = getRandomInt(6) + 1
+        console.log("Rolling die 5")
+    }
+    if (!roll6Saved) {
+        roll6 = getRandomInt(6) + 1
+        console.log("Rolling die 6")
+    }
+    
     if (roll1 === 1) {
-        document.getElementById("die1").src="dice-six-faces-one.png"
+        document.getElementById("die1A").src="dice-six-faces-one.png"
     } else if (roll1 === 2) {
-        document.getElementById("die1").src="dice-six-faces-two.png"
+        document.getElementById("die1A").src="dice-six-faces-two.png"
     } else if (roll1 === 3) {
-        document.getElementById("die1").src="dice-six-faces-three.png"
+        document.getElementById("die1A").src="dice-six-faces-three.png"
     } else if (roll1 === 4) {
-        document.getElementById("die1").src="dice-six-faces-four.png"
+        document.getElementById("die1A").src="dice-six-faces-four.png"
     } else if (roll1 === 5) {
-        document.getElementById("die1").src="dice-six-faces-five.png"
+        document.getElementById("die1A").src="dice-six-faces-five.png"
     } else if (roll1 === 6) {
-        document.getElementById("die1").src="dice-six-faces-six.png"
+        document.getElementById("die1A").src="dice-six-faces-six.png"
     }
     if (roll2 === 1) {
-        document.getElementById("die2").src="dice-six-faces-one.png"
+        document.getElementById("die2A").src="dice-six-faces-one.png"
     } else if (roll2 === 2) {
-        document.getElementById("die2").src="dice-six-faces-two.png"
+        document.getElementById("die2A").src="dice-six-faces-two.png"
     } else if (roll2 === 3) {
-        document.getElementById("die2").src="dice-six-faces-three.png"
+        document.getElementById("die2A").src="dice-six-faces-three.png"
     } else if (roll2 === 4) {
-        document.getElementById("die2").src="dice-six-faces-four.png"
+        document.getElementById("die2A").src="dice-six-faces-four.png"
     } else if (roll2 === 5) {
-        document.getElementById("die2").src="dice-six-faces-five.png"
+        document.getElementById("die2A").src="dice-six-faces-five.png"
     } else if (roll2 === 6) {
-        document.getElementById("die2").src="dice-six-faces-six.png"
+        document.getElementById("die2A").src="dice-six-faces-six.png"
     }
-    rollTotEl.innerHTML = `Roll total: ${score}`
-    console.log("Rolling dice")
-    console.log(score)
+    if (roll3 === 1) {
+        document.getElementById("die3A").src="dice-six-faces-one.png"
+    } else if (roll3 === 2) {
+        document.getElementById("die3A").src="dice-six-faces-two.png"
+    } else if (roll3 === 3) {
+        document.getElementById("die3A").src="dice-six-faces-three.png"
+    } else if (roll3 === 4) {
+        document.getElementById("die3A").src="dice-six-faces-four.png"
+    } else if (roll3 === 5) {
+        document.getElementById("die3A").src="dice-six-faces-five.png"
+    } else if (roll3 === 6) {
+        document.getElementById("die3A").src="dice-six-faces-six.png"
+    }
+    if (roll4 === 1) {
+        document.getElementById("die4A").src="dice-six-faces-one.png"
+    } else if (roll4 === 2) {
+        document.getElementById("die4A").src="dice-six-faces-two.png"
+    } else if (roll4 === 3) {
+        document.getElementById("die4A").src="dice-six-faces-three.png"
+    } else if (roll4 === 4) {
+        document.getElementById("die4A").src="dice-six-faces-four.png"
+    } else if (roll4 === 5) {
+        document.getElementById("die4A").src="dice-six-faces-five.png"
+    } else if (roll4 === 6) {
+        document.getElementById("die4A").src="dice-six-faces-six.png"
+    }
+    if (roll5 === 1) {
+        document.getElementById("die5A").src="dice-six-faces-one.png"
+    } else if (roll5 === 2) {
+        document.getElementById("die5A").src="dice-six-faces-two.png"
+    } else if (roll5 === 3) {
+        document.getElementById("die5A").src="dice-six-faces-three.png"
+    } else if (roll5 === 4) {
+        document.getElementById("die5A").src="dice-six-faces-four.png"
+    } else if (roll5 === 5) {
+        document.getElementById("die5A").src="dice-six-faces-five.png"
+    } else if (roll5 === 6) {
+        document.getElementById("die5A").src="dice-six-faces-six.png"
+    }
+    if (roll6 === 1) {
+        document.getElementById("die6A").src="dice-six-faces-one.png"
+    } else if (roll6 === 2) {
+        document.getElementById("die6A").src="dice-six-faces-two.png"
+    } else if (roll6 === 3) {
+        document.getElementById("die6A").src="dice-six-faces-three.png"
+    } else if (roll6 === 4) {
+        document.getElementById("die6A").src="dice-six-faces-four.png"
+    } else if (roll6 === 5) {
+        document.getElementById("die6A").src="dice-six-faces-five.png"
+    } else if (roll6 === 6) {
+        document.getElementById("die6A").src="dice-six-faces-six.png"
+    }
+    //console.log("Rolling dice")
 }
 
-function resetRoll () {
-    die1El.classList.add("remove")
-    die2El.classList.add("remove")
-    resetScoreEl.classList.add("remove")
-    rollTotEl.innerHTML = `Roll total: No Roll Yet`
+function resetAll () {
+    die1AEl.classList.add("remove")
+    die2AEl.classList.add("remove")
+    die3AEl.classList.add("remove")
+    die4AEl.classList.add("remove")
+    die5AEl.classList.add("remove")
+    die6AEl.classList.add("remove")
+    resetAllEl.classList.add("remove")
 }
 
 rollDiceEl.addEventListener("click", rollDice)
 
-resetScoreEl.addEventListener("click", resetRoll)
+resetAllEl.addEventListener("click", resetAll)
 
 
 
